9d034f511cbdcab980444e41e059b97d50424935
Vedant-03 <agrawalvedant03@gmail.com>
2022-11-02 23:29:48 +0530

    gallery
    initial

--- gamingclubiiti/src/Components/Gallery.js
+++ gamingclubiiti/src/Components/Gallery.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import Masony from "react-masonry-component";
+import imag from '../static/potrait_test.jpg';
 
 function Gallery() {
+
+  const Images = [
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+    { imageUrl: imag},
+  ];
+
   return (
-    <div>Gallery</div>
+    <div>
+      <h1 className='uppercase px-24 pb-12 text-gray-300 text-5xl'>Gallery</h1>
+      <div className='flex container-gallery' style={{'flex-wrap': 'wrap'}}>
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+        <img className='gallery-img' src={imag} alt='' />
+      </div>
+    </div>
   );
 }
 
